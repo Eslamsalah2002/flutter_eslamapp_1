b65d13e93951ba8bdf97454ad0fa0c8addd0ac9f
eslam <eslam@gmail.com>
2022-11-09 23:18:03 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FaceBook(),
+    );
+  }
+}
+
+class FaceBook extends StatelessWidget {
+  const FaceBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "facebook",
+          style: TextStyle(
+              color: Color.fromARGB(255, 7, 117, 207),
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+          color: Colors.blue,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+          IconButton(
+            icon: Icon(Icons.message),
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+        ],
+        
+      ),
+    );
+  }
+}
