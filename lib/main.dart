36d0dd3c6da317c2f82038dc360970eec5f600ee
eslam <eslam@gmail.com>
2022-11-17 23:29:30 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -25,136 +25,126 @@ class FaceBook extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "facebook",
-            style: TextStyle(
-                color: Color.fromARGB(255, 7, 117, 207),
-                fontSize: 33,
-                fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.menu),
+      appBar: AppBar(
+        title: Text(
+          "facebook",
+          style: TextStyle(
+              color: Color.fromARGB(255, 7, 117, 207),
+              fontSize: 33,
+              fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+          color: Colors.blue,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
             onPressed: () {},
             color: Colors.blue,
           ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-              color: Colors.blue,
-            ),
-            IconButton(
-              icon: Icon(Icons.message),
-              onPressed: () {},
-              color: Colors.blue,
-            ),
-          ],
-          elevation: 20,
-        ),
-        // -------------------------------------------------------
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(250),
-                  color: Colors.blueGrey,
-                ),
-                child: Text(
-                  "eslam salah",
-                  style: TextStyle(color: Colors.white, fontSize: 44),
-                ),
-                height: 322,
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: 22),
+          IconButton(
+            icon: Icon(Icons.message),
+            onPressed: () {},
+            color: Colors.blue,
+          ),
+        ],
+        elevation: 20,
+      ),
+      // -------------------------------------------------------
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Text(
+                "eslam salah",
+                style: TextStyle(fontSize: 50, color: Colors.white),
               ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
+              alignment: Alignment.center,
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey,
+                  borderRadius: BorderRadius.circular(50)),
+              margin: EdgeInsets.only(bottom: 50),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
                 children: [
-                  
                   Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blueGrey,
-                    ),
-                    // ignore: sort_child_properties_last
                     child: Text(
                       "eslam ",
-                      style: TextStyle(color: Colors.white, fontSize: 27),
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
+                    alignment: Alignment.center,
                     height: 100,
                     width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(15)),
                   ),
                   Container(
-                    margin: EdgeInsets.only(left: 19),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blueGrey,
-                    ),
-                    // ignore: sort_child_properties_last
                     child: Text(
                       "salah ",
-                      style: TextStyle(color: Colors.white, fontSize: 27),
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
+                    alignment: Alignment.center,
                     height: 100,
                     width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(15)),
+                    margin: EdgeInsets.only(left: 19),
                   ),
                   Container(
-                    margin: EdgeInsets.only(left: 19),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blueGrey,
-                    ),
-                    // ignore: sort_child_properties_last
                     child: Text(
                       "abdo ",
-                      style: TextStyle(color: Colors.white, fontSize: 27),
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
+                    alignment: Alignment.center,
                     height: 100,
                     width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(15)),
+                    margin: EdgeInsets.only(left: 19),
                   ),
                   Container(
-                    margin: EdgeInsets.only(left: 19),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(12),
-                      color: Colors.blueGrey,
-                    ),
-                    // ignore: sort_child_properties_last
                     child: Text(
                       "elsadek ",
-                      style: TextStyle(color: Colors.white, fontSize: 27),
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
+                    alignment: Alignment.center,
                     height: 100,
                     width: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadius.circular(15)),
+                    margin: EdgeInsets.only(left: 19),
                   ),
                 ],
               ),
+            ),
+            Container(
+              child: Text(
+                "eslam salah",
+                style: TextStyle(fontSize: 50, color: Colors.white),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(250),
+              alignment: Alignment.center,
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
                   color: Colors.blueGrey,
-                ),
-                child: Text(
-                  "eslam salah",
-                  style: TextStyle(color: Colors.white, fontSize: 44),
-                ),
-                height: 322,
-                width: double.infinity,
-              )
-            ],
-          ),
-        ));
+                  borderRadius: BorderRadius.circular(50)),
+              margin: EdgeInsets.only(top: 50),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
