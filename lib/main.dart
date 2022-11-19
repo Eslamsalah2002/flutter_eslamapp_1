366ed2239cd18a695a0c4edd42436b11f811cd69
eslam <eslam@gmail.com>
2022-11-19 23:02:17 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -114,6 +114,7 @@ class FaceBook extends StatelessWidget {
                     margin: EdgeInsets.only(left: 19),
                   ),
                   Container(
+                    // ignore: sort_child_properties_last
                     child: Text(
                       "elsadek ",
                       style: TextStyle(fontSize: 20, color: Colors.white),
@@ -130,6 +131,7 @@ class FaceBook extends StatelessWidget {
               ),
             ),
             Container(
+              // ignore: sort_child_properties_last
               child: Text(
                 "eslam salah",
                 style: TextStyle(fontSize: 50, color: Colors.white),
@@ -141,10 +143,63 @@ class FaceBook extends StatelessWidget {
                   color: Colors.blueGrey,
                   borderRadius: BorderRadius.circular(50)),
               margin: EdgeInsets.only(top: 50),
+            ),
+            Container(
+              // ignore: sort_child_properties_last
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text("eslam",style: TextStyle(color: Color.fromARGB(255, 47, 33, 243),
+                      fontSize: 30),
+                      ),
+                      color: Colors.amber,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                  
+                   Expanded(
+                    flex: 2,
+                     child: Container(
+                      alignment: Alignment.center,
+                      child: Text("salah",style: TextStyle(color: Color.fromARGB(255, 240, 243, 33),
+                      fontSize: 30),
+                      ),
+                      color: Color.fromARGB(255, 7, 61, 255),
+                      width: 100,
+                      height: 100,
+                                     ),
+                   ),
+                  Expanded(
+                    flex: 1,
+                    child:  Container(
+                    alignment: Alignment.center,
+                    child: Text("abdo",style: TextStyle(color: Color.fromARGB(255, 254, 254, 254),
+                    fontSize: 30),
+                    ),
+                    color: Color.fromARGB(255, 255, 7, 40),
+                    width: 100,
+                    height: 100,
+                    
+                  ),)
+                  
+                ],
+              ),
+              
+              height: 300,
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(15),
+              ),
             )
           ],
         ),
       ),
+    
     );
   }
 }
